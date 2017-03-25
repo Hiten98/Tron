c566a76269e977c4b93adc89260f0aeb8c7c370f
Hiten98 <hitenrathod98@gmail.com>
2017-03-25 14:47:00 -0400

    Working bot with shitty direction...
    Take Mihir's code for direction and implement his logic, but keep my weight logic... Also, mine works as a quick fix, change the values for inside board and crashing from +/-2 to +/-1

--- Bot.java
+++ Bot.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 public class Bot {
 
     //TODO: HERE! IMPORTANT! Input your AUTH_KEY below!
-    final static String AUTH_KEY = "XNQSS1490395092222";
+    final static String AUTH_KEY = "BGEFI1490393931532";
 
     static Integer[][] gameBoard;
     static int myPlayerNumber;
@@ -20,12 +20,22 @@ public class Bot {
       Start writing your code here!
       ===========================================*/
     public static String makeMove() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                if(gameBoard[i][j] == myPlayerNumber)
+                {
+                    myCurrentRow = i;
+                    myCurrentColumn = j;
+                }
+            }
+        }
         if (gameBoard == null || gameBoard.length == 0 || gameBoard[0].length == 0) {
             System.out.println("Game board was messed up coming into public void makeMove()");
             return "DOWN";
         }
         int[] moves = Weight.calculateWeight(); //0-up, 1-down, 2-left, 3-right
-        int dir = getMaxIndex(moves);
+        int dir;
+        dir = getMaxIndex(moves);
         String dirn = "";
         if (dir == 0)
             dirn = "UP";
@@ -35,57 +45,44 @@ public class Bot {
             dirn = "LEFT";
         else
             dirn = "RIGHT";
-
-        if (dirn.equals("UP") && !isInsideBoard(myCurrentRow - 1, myCurrentColumn)){
-            moves[dir] = -15000;
-            dir = getMaxIndex(moves);
-            if (dir == 1)
-                return "DOWN";
-            else if (dir == 2)
-                return "LEFT";
-            else
-                return "RIGHT";
-        }
-        else if (dirn.equals("DOWN") && !isInsideBoard(myCurrentRow + 1, myCurrentColumn)){
-            moves[dir] = -15000;
-            dir = getMaxIndex(moves);
-            if (dir == 0)
-                return "UP";
-            else if (dir == 2)
-                return "LEFT";
-            else
-                return "RIGHT";
-        }
-        else if (dirn.equals("LEFT") && !isInsideBoard(myCurrentRow, myCurrentColumn - 1)){
-            moves[dir] = -15000;
-            dir = getMaxIndex(moves);
-            if (dir == 0)
-                return "UP";
-            else if (dir == 1)
-                return "DOWN";
-            else
-                return "RIGHT";
-        }
-        else if (dirn.equals("RIGHT") && !isInsideBoard(myCurrentRow, myCurrentColumn + 1)){
-            moves[dir] = -15000;
-            dir = getMaxIndex(moves);
-            if (dir == 0)
-                return "UP";
-            else if (dir == 1)
-                return "DOWN";
-            else
-                return "LEFT";
-        }
-        else {
-            if (dir == 0)
-                return "UP";
-            else if (dir == 1)
-                return "DOWN";
-            else if (dir == 2)
-                return "LEFT";
-            else
-                return "RIGHT";
-        }
+        System.out.println(myCurrentColumn + "\n" + myCurrentRow + "\n" + myPlayerNumber);
+        if(dirn.equals("UP") && !isInsideBoard(myCurrentRow -2,myCurrentColumn) && gameBoard[myCurrentRow-2][myCurrentColumn] != 0)
+            dirn = "RIGHT";
+        if(dirn.equals("DOWN")&& !isInsideBoard(myCurrentRow +2,myCurrentColumn) && gameBoard[myCurrentRow + 2][myCurrentColumn] != 0)
+            dirn = "LEFT";
+        if (dirn.equals("RIGHT") && !isInsideBoard(myCurrentRow,myCurrentColumn + 2) && gameBoard[myCurrentRow][myCurrentColumn + 2] != 0)
+            dirn = "UP";
+        if(dirn.equals("LEFT") && !isInsideBoard(myCurrentRow,myCurrentColumn - 2) && gameBoard[myCurrentRow][myCurrentColumn-2] != 0)
+            dirn = "DOWN";
+        System.out.println(myCurrentColumn + "\n" + myCurrentRow + "\n" + myPlayerNumber);
+        if(dirn.equals("UP") && !isInsideBoard(myCurrentRow -2,myCurrentColumn) && gameBoard[myCurrentRow-2][myCurrentColumn] != 0)
+            dirn = "RIGHT";
+        if(dirn.equals("DOWN")&& !isInsideBoard(myCurrentRow +2,myCurrentColumn) && gameBoard[myCurrentRow + 2][myCurrentColumn] != 0)
+            dirn = "LEFT";
+        if (dirn.equals("RIGHT") && !isInsideBoard(myCurrentRow,myCurrentColumn + 2) && gameBoard[myCurrentRow][myCurrentColumn + 2] != 0)
+            dirn = "UP";
+        if(dirn.equals("LEFT") && !isInsideBoard(myCurrentRow,myCurrentColumn - 2) && gameBoard[myCurrentRow][myCurrentColumn-2] != 0)
+            dirn = "DOWN";
+        System.out.println(myCurrentColumn + "\n" + myCurrentRow + "\n" + myPlayerNumber);
+        if(dirn.equals("UP") && !isInsideBoard(myCurrentRow -2,myCurrentColumn) && gameBoard[myCurrentRow-2][myCurrentColumn] != 0)
+            dirn = "RIGHT";
+        if(dirn.equals("DOWN")&& !isInsideBoard(myCurrentRow +2,myCurrentColumn) && gameBoard[myCurrentRow + 2][myCurrentColumn] != 0)
+            dirn = "LEFT";
+        if (dirn.equals("RIGHT") && !isInsideBoard(myCurrentRow,myCurrentColumn + 2) && gameBoard[myCurrentRow][myCurrentColumn + 2] != 0)
+            dirn = "UP";
+        if(dirn.equals("LEFT") && !isInsideBoard(myCurrentRow,myCurrentColumn - 2) && gameBoard[myCurrentRow][myCurrentColumn-2] != 0)
+            dirn = "DOWN";
+        System.out.println(myCurrentColumn + "\n" + myCurrentRow + "\n" + myPlayerNumber);
+        if(dirn.equals("UP") && !isInsideBoard(myCurrentRow -2,myCurrentColumn) && gameBoard[myCurrentRow-2][myCurrentColumn] != 0)
+            dirn = "RIGHT";
+        if(dirn.equals("DOWN")&& !isInsideBoard(myCurrentRow +2,myCurrentColumn) && gameBoard[myCurrentRow + 2][myCurrentColumn] != 0)
+            dirn = "LEFT";
+        if (dirn.equals("RIGHT") && !isInsideBoard(myCurrentRow,myCurrentColumn + 2) && gameBoard[myCurrentRow][myCurrentColumn + 2] != 0)
+            dirn = "UP";
+        if(dirn.equals("LEFT") && !isInsideBoard(myCurrentRow,myCurrentColumn - 2) && gameBoard[myCurrentRow][myCurrentColumn-2] != 0)
+            dirn = "DOWN";
+        System.out.print(dirn);
+        return dirn;
         //Return UP, DOWN, LEFT, or RIGHT to move that direction
         //Best of luck!
     }
@@ -94,7 +91,7 @@ public class Bot {
         int max = 0;
 
         for(int i = 1; i < moves.length; i++){
-            if (moves[max] < moves[i])
+            if (moves[max] > moves[i])
                 max = i;
         }
 
@@ -103,7 +100,7 @@ public class Bot {
 
     //Small helper Method
     public static boolean isInsideBoard(int i, int j) {
-        if (i > gameBoard.length || j > gameBoard.length) {
+        if (i >= gameBoard.length || j >= gameBoard.length) {
             return false;
         } else if (i < 0 || j < 0) {
             return false;
